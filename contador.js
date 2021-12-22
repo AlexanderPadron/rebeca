dd5ca3665032f4bc6ec92a0bd91d6a9c9a82f106
Alex <alex0195@gmail.com>
2021-12-22 18:40:42 -0300

    pasamos a vue

--- contador.js
+++ contador.js
@@ -1,20 +1,3 @@
-const test = {
-    prop: 42,
-    func: function() {
-        let prop = 40;
-      return this.prop;
-    },
-  };
-  
-  console.log(test.func());
-// for (let i = 0; i < numeros.length; i++) {
-//     console.log(numeros[i]);
-// }
+const x = 'AAAA';
 
-// for(let num of numeros){
-//     console.log(num);
-// }
-
-// numeros.forEach(function(numero){
-//     console.log(numero);
-// })
+console.log(JSON.stringify(x));
